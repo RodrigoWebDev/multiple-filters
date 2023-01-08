6d92218e18c98d38809807679c0a7baead8682d8
Rodrigo Queiroz <rodrigo.queiroz@dbccompany.com.br>
2023-01-08 13:39:50 -0300

    build command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css"
-import "./index.css"
 
 const container = document.getElementById("app");
 const root = createRoot(container)
